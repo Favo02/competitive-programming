a8e7299d98351cd370c58c25c59c2c29431d89cd
Luca Favini <luca.favini.02@gmail.com>
2023-10-20 15:13:28 +0200

    cloudflight-coding-contest: level1 working
    Co-authored-by: Andrea Sacchi <alsacchi@users.noreply.github.com>

--- cloudflight-coding-contest/38th-CCC-October-2023/1.py
+++ cloudflight-coding-contest/38th-CCC-October-2023/1.py
@@ -1,3 +1,15 @@
-f = open("file.txt", "r")
-lines = f.readlines()
-print(lines)
+import sys
+
+input = sys.argv[1]
+f = open(input, "r")
+lines = list(map(lambda l: l[:-1], f.readlines()))
+gridSize = int(lines[0])
+grid = lines[1:1+gridSize]
+coords = list(map(lambda t: t.split(","), lines[1+gridSize:][1:]))
+# print(grid)
+# print(coords[1:])
+for c in coords:
+  a = int(c[0])
+  b = int(c[1])
+  res = grid[b][a]
+  print(res)
