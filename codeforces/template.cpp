c1541798b8440885118d9aca091b14539c1ab942
Luca Favini <luca.favini.02@gmail.com>
2023-11-26 18:44:14 +0100

    codeforces: add `c++` `template` and `utils`

---
+++ codeforces/template.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+
+void solve() {
+  int a, b;
+  cin >> a >> b;
+  // solve...
+}
+
+int main() {
+  int cases;
+  cin >> cases;
+  while (cases-- > 0) solve();
+}
