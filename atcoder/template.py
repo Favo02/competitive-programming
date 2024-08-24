266517850a665ba469b0911763ac2282cd96b60f
Luca Favini <luca.favini.02@gmail.com>
2024-08-24 17:29:17 +0200

    atcoder: update `python` template

--- atcoder/template.py
+++ atcoder/template.py
@@ -1,3 +1,4 @@
+# from collections import deque
 # from collections import Counter
 # from collections import defaultdict
 # import heapq
@@ -8,27 +9,19 @@
 
 # sys.setrecursionlimit(10**5)
 
-def solve():
+# lenght + list of numbers
+LEN = int(input())
+nums = [int(n) for n in input().split()]
+print(LEN, nums)
 
-  # lenght + list of numbers
-  LEN = int(input())
-  nums = [int(n) for n in input().split()]
-  print(LEN, nums)
+# lenght + N lines of 2 numbers
+LEN = int(input())
+nums = []
+for _ in range(LEN):
+  a, b = input().split()
+  nums.append((int(a), int(b)))
+print(LEN, nums)
 
-  # lenght + N lines of 2 numbers
-  LEN = int(input())
-  nums = []
-  for _ in range(LEN):
-    a, b = input().split()
-    nums.append((int(a), int(b)))
-  print(LEN, nums)
-
-  # string
-  string = input()
-  print(string)
-
-  return -1
-
-cases = int(input())
-for _ in range(cases):
-  print(solve())
+# string
+string = input()
+print(string)
