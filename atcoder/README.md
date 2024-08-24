14547c1147abd211e8205af252b4330f1828f106
Luca Favini <luca.favini.02@gmail.com>
2024-08-24 12:11:27 +0200

    atcoder: repo init: add templates and readme

---
+++ atcoder/README.md
@@ -0,0 +1,8 @@
+## Codeforces
+
+My solutions for [AtCoder](https://atcoder.jp/) (competitive programming) contests.
+
+> AtCoder is a programming contest site for anyone from beginners to experts. We hold weekly programming contests online.
+
+## Contest results
+
---
+++ atcoder/template.py
@@ -0,0 +1,34 @@
+# from collections import Counter
+# from collections import defaultdict
+# import heapq
+# import bisect
+# from functools import lru_cache
+# import math
+# import sys
+
+# sys.setrecursionlimit(10**5)
+
+def solve():
+
+  # lenght + list of numbers
+  LEN = int(input())
+  nums = [int(n) for n in input().split()]
+  print(LEN, nums)
+
+  # lenght + N lines of 2 numbers
+  LEN = int(input())
+  nums = []
+  for _ in range(LEN):
+    a, b = input().split()
+    nums.append((int(a), int(b)))
+  print(LEN, nums)
+
+  # string
+  string = input()
+  print(string)
+
+  return -1
+
+cases = int(input())
+for _ in range(cases):
+  print(solve())
