9f88893dae70d2175a941d1dc5ad3da2f44b6ba6
Luca Favini <luca.favini.02@gmail.com>
2024-11-26 16:06:26 +0100

    cses: rename files

--- cses/4-GraphAlgorithms/1669.py
+++
@@ -1,9 +0,0 @@
-cities, roads = map(int, input().split())
-
-graph = [[] for _ in range(cities)]
-for _ in range(roads):
-  f, t = map(int, input().split())
-  graph[f-1].append(t-1)
-  graph[t-1].append(f-1)
-
-print(graph)
