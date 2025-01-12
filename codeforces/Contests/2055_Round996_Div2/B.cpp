21ee6a09bff43b2ba8e0ce99cd032022101d8935
Luca Favini <luca.favini.02@gmail.com>
2025-01-12 18:13:05 +0100

    codeforces: add `2055_Round996_Div2` solutions

---
+++ codeforces/Contests/2055_Round996_Div2/B.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx,avx2,fma")
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+
+#define PI 3.1415926535897932384626433832795l
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+#define int long long
+
+bool valid(vector<int> &cur, vector<int> &target, int n) {
+    int debt = 0;
+    int margin = 1e9 + 10;
+
+    for (int i = 0; i < n; i++) {
+        if (cur[i] >= target[i]) {
+            margin = min(margin, cur[i] - target[i]);
+        }
+        // debt needed
+        else {
+            if (debt != 0) return false;
+            debt = target[i] - cur[i];
+        }
+    }
+
+    return margin >= debt;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+
+    vector<int> cur(n);
+    vector<int> tar(n);
+
+    for (int i = 0; i < n; i++) cin >> cur[i];
+    for (int i = 0; i < n; i++) cin >> tar[i];
+
+    // dbg(cur);
+    // dbg(tar);
+
+    if (valid(cur, tar, n)) cout << "YES" << endl;
+    else cout << "NO" << endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int tc = 1;
+    cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        // cout << "Case #" << t << endl;
+        solve();
+    }
+}
